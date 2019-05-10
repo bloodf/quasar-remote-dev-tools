4776a82ef8aa69c4956137e2e747e34433eac529
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-05-10 17:20:53 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const ip = require('ip');
 
 const { spawn } = require('child_process'),
 
-const extendConf = function(conf) {
+function extendConf(conf) {
   conf.boot.push('~quasar-app-extension-remote-dev-tools/src/boot/devtools.js');
   console.log(` App Extension (devtools) Info: 'Adding devtools boot reference to your quasar.conf.js'`);
   conf.build.transpileDependencies.push(/quasar-app-extension-remote-dev-tools[\\/]src/);
