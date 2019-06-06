86a613fcc08c171532c2998f06f9ca08e9bc2be8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-06-06 17:16:39 -0300

    added env to control if you want to launch remote devtools

--- src/index.js
+++ src/index.js
@@ -2,25 +2,28 @@ const ip = require('ip');
 
 const { spawn } = require('child_process');
 
-function extendConf(conf) {
-  conf.boot.push('~quasar-app-extension-remote-dev-tools/src/boot/devtools.js');
-  console.log(` App Extension (devtools) Info: 'Adding devtools boot reference to your quasar.conf.js'`);
-  conf.build.transpileDependencies.push(/quasar-app-extension-remote-dev-tools[\\/]src/);
-  conf.build.env = {
-    ...conf.build.env,
-    DEVTOOLS_IP: JSON.stringify(ip.address()),
-    DEVTOOLS: true,
-  };
-}
-
-module.exports = function(api) {
+module.exports = (api, ctx) => {
+  let runDevTools = false;
   api.compatibleWith('@quasar/app', '^1.0.0-beta.18');
+  api.extendQuasarConf((conf, api) => {
+    conf.build.env = {
+      DEVTOOLS_IP: JSON.stringify(ip.address()),
+      DEVTOOLS: true,
+      ...conf.build.env,
+    };
 
-  if (api.ctx.dev === true && (api.ctx.mode.electron || api.ctx.mode.cordova)) {
-    api.extendQuasarConf(extendConf);
-    api.beforeDev(() => {
+    if(conf.build.env.DEVTOOLS){
+      conf.boot.push('~quasar-app-extension-remote-dev-tools/src/boot/devtools.js');
+      console.log(` App Extension (remote-devtools) Info: 'Adding devtools boot reference to your quasar.conf.js'`);
+      conf.build.transpileDependencies.push(/quasar-app-extension-remote-dev-tools[\\/]src/);
+      runDevTools = true;
+    }
+  });
+
+  api.beforeDev(() => {
+    if (runDevTools) {
       console.log(`Opening vue-devtools for your IP: ${ip.address()}`);
       spawn('npx', ['vue-devtools']);
-    });
-  }
+    }
+  });
 };
